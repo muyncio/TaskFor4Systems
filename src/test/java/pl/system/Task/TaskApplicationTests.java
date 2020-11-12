d8f03ff499af233b015a3c22f3493cd720973fb7
muyncio <muynek@gmail.com>
2020-11-12 13:38:56 +0100

    Initial commit

---
+++ src/test/java/pl/system/Task/TaskApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.system.Task;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
